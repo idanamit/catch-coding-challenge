40693483459ef92b0ce4f79e3e7e9b88c8e8c69a
idanamit <idanamit4@gmail.com>
2019-11-27 15:47:25 +0200

    First commit Started working on the assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/header/Header";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       Template Project
+        <Header query={"test"} total={100} currPage={1} pages={4}/>
     </div>
   );
 };
