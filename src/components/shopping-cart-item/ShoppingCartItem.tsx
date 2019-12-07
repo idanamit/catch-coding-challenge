7623cc4cb852b48d8086d80b6094f3c87d369f66
Idan Amit <idanamit4@gmail.com>
2019-12-07 16:52:08 +1100

    Added add, remove and delete buttons actions Added missing key property in shopping cart loop

--- src/components/shopping-cart-item/ShoppingCartItem.tsx
+++ src/components/shopping-cart-item/ShoppingCartItem.tsx
@@ -3,12 +3,38 @@ import {CartProduct} from "../../Interfaces";
 import './ShoppingCartItem.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus, faMinus, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {useDispatch} from "react-redux";
+import {constants} from "../../Constants";
 
 
 const ShoppingCartItem: React.FC<{
     product: CartProduct;
     amount: number
 }> = (props) => {
+    const dispatch = useDispatch();
+
+    const changeAmount = (isIncrease) => {
+        if (isIncrease) {
+          dispatch({
+              type: constants.actions.ADD_TO_CART,
+              payload: props.product
+          })
+        }
+        else {
+            dispatch({
+                type: constants.actions.DECREASE_PRODUCT_AMOUNT,
+                payload: props.product
+            })
+        }
+    };
+
+    const removeProduct = () => {
+        dispatch({
+            type: constants.actions.REMOVE_PRODUCT,
+            payload: props.product
+        })
+    };
+
     return (
         <div className="cart-product-container">
             <div className='cart-product-information'>
@@ -17,13 +43,13 @@ const ShoppingCartItem: React.FC<{
                 <div className="cart-product-price">{props.product.price / 100 + "$"}</div>
             </div>
             <div className='cart-product-buttons-container'>
-                <button className="cart-product-button btn">
+                <button className="cart-product-button btn" onClick={() => {changeAmount(true)}}>
                     <FontAwesomeIcon icon={faPlus}/>
                 </button>
-                <button className="cart-product-button btn">
+                <button className="cart-product-button btn" onClick={() => {changeAmount(false)}}>
                     <FontAwesomeIcon icon={faMinus}/>
                 </button>
-                <button className="cart-product-button btn">
+                <button className="cart-product-button btn" onClick={removeProduct}>
                     <FontAwesomeIcon icon={faTrash}/>
                 </button>
             </div>
