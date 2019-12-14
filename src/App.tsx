5bd475723f0bda52905035db4f89fb116b7933bd
Idan Amit <idanamit4@gmail.com>
2019-12-14 17:14:02 +1100

    Fixed missing count items in the product list heading Added shrinking sticky header Added fixed position on the shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,21 @@ import useGetProductListData from "./services/GetData.service";
 import ProductListHeading from "./components/product-list-heading/ProductListHeading";
 import ProductCardList from "./components/product-card-list/ProductCardList";
 import ShoppingCart from "./components/shopping-cart/ShoppingCart";
+import HeaderFunctions from "./HeaderFunctions";
 
 const App: React.FC = () => {
     const service = useGetProductListData();
+    HeaderFunctions();
 
     return (
         <div className="App">
-            <div className="page-header">
-                Template Project
+            <div className="page-header" id="page-header">
+                Catch Coding Challenge
             </div>
             {service && service.metadata && service.results &&
             <div className="page-content">
                 <div className="heading-container">
-                    <ProductListHeading metadata={service.metadata}/>
+                    <ProductListHeading metadata={service.metadata} countItems={service.results.length}/>
                 </div>
                 <div className="product-list-container">
                     <ProductCardList results={service.results}/>
